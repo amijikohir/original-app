6c77efbedf7eb986183775712dc84951b6a4bf7a
amijikohir <rihorira.0625@gmail.com>
2023-11-16 18:49:43 +0900

    Revert "categoriesテーブルの作成"
    This reverts commit a748efd19a74e5bbbf274cd919e4d119940fcc38.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_093056) do
-  create_table "categories", charset: "utf8", force: :cascade do |t|
-    t.string "content", null: false
-    t.integer "amount", null: false
-    t.string "memo"
-    t.bigint "room_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["room_id"], name: "index_categories_on_room_id"
-    t.index ["user_id"], name: "index_categories_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_085247) do
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
@@ -51,8 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_093056) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "categories", "rooms"
-  add_foreign_key "categories", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
